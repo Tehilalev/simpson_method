5df5210a153b9bb00802fdddc4678961cbf10741
Tehila <94409393+Tehilalev@users.noreply.github.com>
2022-06-08 01:35:48 +0300

    .

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ for j in array_max:
 
 error = 0.00558
 print(f'Error with {num_sections} sections: '
-      f'{simpson.calculate_error_range(f_prime4(func_(max_)), x0, x1, num_sections)}\n')
+      f'{simpson.calculate_error_range(f_prime4(func_(max_)), x0, x1, num_sections)}:.4f\n')
 
 # sections calculations
 print(f'Amount of sections with error {error}: '
